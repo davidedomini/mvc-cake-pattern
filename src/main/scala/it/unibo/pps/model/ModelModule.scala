ad9b87ac3172fae51d05e8c43b0d420a4f7e4f87
dom <davidedomini@gmail.com>
2022-07-16 16:09:03 +0200

    implemented ModelModule

---
+++ src/main/scala/it/unibo/pps/model/ModelModule.scala
@@ -0,0 +1,21 @@
+package it.unibo.pps.model
+
+object ModelModule:
+  trait Model:
+    def init(): Unit
+    def updateVirtualTime(): Unit
+    def getVirtualTime(): Int
+
+  trait Provider:
+    val model: Model
+
+  trait Component:
+    class ModelImpl extends Model:
+
+      var virtualTime: Int = 0
+
+      def init(): Unit = virtualTime = 0
+      def updateVirtualTime(): Unit = virtualTime = virtualTime + 1
+      def getVirtualTime(): Int = virtualTime
+
+  trait Interface extends Provider with Component
